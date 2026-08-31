a68d2856a1ee6f18e8105d1c46a600b54841f82e
Madhuka Fernando <info.madhukafdo@gmail.com>
2026-09-01 05:14:28 +0530

    feat: Connect DB

---
+++ src/db/db.js
@@ -0,0 +1,8 @@
+// Import the PrismaClient Class
+import { PrismaClient } from "@prisma/client";
+
+//Create PrismaClient Object
+const prisma = new PrismaClient();
+
+// Export the PrismaClient Object
+export default prisma;
